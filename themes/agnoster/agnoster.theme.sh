3503d645536b427bc9dd9e3028bf4f2edcde831b
Hosameldin <hosameldin.mohamed@iit.it>
2022-10-24 18:30:54 +0200

    Conda python symbol

--- themes/agnoster/agnoster.theme.sh
+++ themes/agnoster/agnoster.theme.sh
@@ -273,7 +273,7 @@ prompt_dir() {
 prompt_conda() {
     if [ ! -z "$CONDA_DEFAULT_ENV" ]; then
         if [ "$CONDA_DEFAULT_ENV" != "base" ]; then
-            prompt_segment blue black "$CONDA_DEFAULT_ENV"
+            prompt_segment blue black " $CONDA_DEFAULT_ENV"
         fi
     fi
 }
