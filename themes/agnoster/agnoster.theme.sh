57b75afb5b73055f2a46dd0501285c0e2807298d
Hosameldin <hosameldin.mohamed@iit.it>
2022-11-05 02:21:54 +0100

    revert conda symbol color

--- themes/agnoster/agnoster.theme.sh
+++ themes/agnoster/agnoster.theme.sh
@@ -273,8 +273,7 @@ prompt_dir() {
 prompt_conda() {
     if [ ! -z "$CONDA_DEFAULT_ENV" ]; then
         if [ "$CONDA_DEFAULT_ENV" != "base" ]; then
-            prompt_segment black yellow " "
-            prompt_segment blue black "$CONDA_DEFAULT_ENV"
+            prompt_segment blue black " $CONDA_DEFAULT_ENV"
         fi
     fi
 }
